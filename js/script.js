18911b2652df89d5a01f6abfa4d80e2cff195193
annafricks <annafricks33@gmail.com>
2022-12-16 12:50:22 -0500

    Changed to pokemonList with 6 pokemon  along with details of each

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-alert('Hello World');
-
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
+    {name: 'Squirtle', height: 0.5, type: ['Water']},
+    {name: 'Charmander', height: 0.6, type: ['Fire']},
+    {name: 'Vulpix', height: 0.6, type: ['Fire']},
+    {name: 'Eevee', height: 0.3, type: ['Normal']},
+    {name: 'Jolteon', height: 0.8, type: ['Electric']},
+]
