165bf72873a696754b32afa3ded22e6a4f0fcdd3
annafricks <annafricks33@gmail.com>
2023-01-19 23:39:56 -0500

    added ArrayDetails and another pokemonList (incomplete)

--- js/script.js
+++ js/script.js
@@ -7,6 +7,35 @@ const pokemonList = [
     {name: 'Jolteon', height: 0.8, type: ['Electric']},
 ];
 
+const pokemonList2 = [
+    //different set of Pokemon objects
+];
+
+function printArrayDetails(list){
+    for ( const i = 0; i < list.length; i++){
+        document.write("<p> + list[i].name + "</p>);
+        //printing list[i]'s other details
+        //...
+    }
+}
+//"list" in code above is to be filled- it acts as a placeholder
+
+printArrayDetails(pokemonList); //executes the function using 'pokemonList' as its input
+printArrayDetails(pokemonList2); //executes the function using 'pokemonList2' as its input
+
+function divide(dividend, divisor){
+    if(divisor === 0){
+        return "You're trying to divide by zero."
+    }else{
+        let result = dividend / divisor;
+        return result;
+    }
+}
+ console.log(divide(4, 2));
+ console.log(divide(7, 0));
+ console.log(divide(1, 4));
+ console.log(divide(12, -3));
+ 
 for (const i = 0; i < pokemonList.height; i++) {
     if ( pokemonList[i].height <=0.6 ) && ( pokemonList[i].height >=0.7 ) {
     document.write(height[i].pokemonList + " Is tall ")
